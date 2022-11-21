f1d991cc2444b413277e2216c493478c11d5c78b
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-21 19:46:13 -0300

    feat: add input handlers to AddUser comp

--- src/components/User/AddUser.jsx
+++ src/components/User/AddUser.jsx
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 
 function AddUser() {
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
+
+  const handleUsernameInput = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleAgeInput = (e) => {
+    setAge(e.target.value);
+  };
+
   const handleAddUser = (e) => {
     e.preventDefault();
+
+    if (username.trim().length === 0 || age.trim().length === 0) return;
+    if (+age < 0) return;
+
+    console.log(username, age);
+
+    setAge('');
+    setUsername('');
   };
 
   return (
     <Card>
       <form onSubmit={handleAddUser} className='flex flex-col gap-2'>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' className='border' />
+        <input
+          type='text'
+          id='username'
+          className='border-2'
+          onChange={handleUsernameInput}
+          value={username}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input type='number' id='age' className='border' />
+        <input
+          type='number'
+          id='age'
+          className='border-2'
+          onChange={handleAgeInput}
+          value={age}
+        />
         <button type='submit'>Add User</button>
       </form>
     </Card>
