fa6183cc080cd094d2a81cd014185a15d32939af
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-21 21:17:58 -0300

    feat: add handleAddUser in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import AddUser from './components/User/AddUser';
+import UsersList from './components/User/UsersList';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleAddUser = (user) => {
+    setUsers((prevState) => [...prevState, user]);
+  };
+
   return (
     <div className='App'>
-      <AddUser />
+      <AddUser handleAddUser={handleAddUser} />
+      {users.length > 0 && <UsersList users={users} />}
     </div>
   );
 }
