0d5cd6263dd328b3e85dcd32fa379bbc8ad7629d
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-11-22 02:59:53 -0300

    feat: add error logic to AddUser comp

--- src/components/User/AddUser.jsx
+++ src/components/User/AddUser.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
 
 function AddUser({ handleAddUser }) {
   const [username, setUsername] = useState('');
   const [age, setAge] = useState('');
+  const [error, setError] = useState();
 
   const handleUsernameInput = (e) => {
     setUsername(e.target.value);
@@ -16,8 +18,23 @@ function AddUser({ handleAddUser }) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (username.trim().length === 0 || age.trim().length === 0) return;
-    if (+age < 0) return;
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).',
+      });
+
+      return;
+    }
+
+    if (+age < 0) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).',
+      });
+
+      return;
+    }
 
     handleAddUser({ name: username, age });
 
@@ -26,31 +43,34 @@ function AddUser({ handleAddUser }) {
   };
 
   return (
-    <Card>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
-        <label htmlFor='username'>Username</label>
-        <input
-          type='text'
-          id='username'
-          className='border-2'
-          onChange={handleUsernameInput}
-          value={username}
-        />
-        <label htmlFor='age'>Age (Years)</label>
-        <input
-          type='number'
-          id='age'
-          className='border-2'
-          onChange={handleAgeInput}
-          value={age}
-        />
-        <button
-          type='submit'
-          className='place-self-center border px-2 py-1 rounded-md text-white font-medium bg-black'>
-          Add User
-        </button>
-      </form>
-    </Card>
+    <>
+      <ErrorModal error={error} />
+      <Card>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            id='username'
+            className='border-2'
+            onChange={handleUsernameInput}
+            value={username}
+          />
+          <label htmlFor='age'>Age (Years)</label>
+          <input
+            type='number'
+            id='age'
+            className='border-2'
+            onChange={handleAgeInput}
+            value={age}
+          />
+          <button
+            type='submit'
+            className='place-self-center border px-2 py-1 rounded-md text-white font-medium bg-black'>
+            Add User
+          </button>
+        </form>
+      </Card>
+    </>
   );
 }
 
